37051cbb46b55e514b6246198360a4c7d763c107
Ted <fgordy@gmail.com>
2018-10-11 09:11:55 +0300

    Добавил

--- cgi-bin/st10/main.py
+++ cgi-bin/st10/main.py
@@ -1,32 +1,21 @@
 from Container import Container
 
-container=Container()
+def main(q,selfurl):
+container = Container()
 
-
-MENU=[
-    ["Добавить сотрудника", container.addworker],
-    ["Добавиь главу отдела", container.addhead],
-    ["Редактировать элемент", container.edittarget],
-    ["Вывести весь список", container.show],
-    ["Удалить список", container.clearcontainer],
-    ["Считать список из файла", container.fromfile],
-    ["Записать список в файл", container.infile],
-    ["Выход"]
-]
-
-def main():
-    while True:
-        for i,item in enumerate(MENU):
-            print("{0:2}.{1}".format(i,item[0]))
-        print("Выберите действие")
-        flag=int(input())
-        if flag > 7:
-            print("Некорректный ввод")
-        elif flag == 7:
-            break
-        else:
-            MENU[flag][1]()
-
-
-if __name__ == "__main__":
-    main()
+    MENU = [
+        ["Добавить сотрудника", container.addworker],
+        ["Добавиь главу отдела", container.addhead],
+        ["Редактировать элемент", container.edittarget],
+        ["Вывести весь список", container.show],
+        ["Удалить элемент",container.delete_target],
+        ["Удалить список", container.clearcontainer],
+        ["Считать список из файла", container.fromfile],
+        ["Записать список в файл", container.infile],
+        ["Принять значение", container.get]
+    ]
+    print("Content-type: text/html; charset=utf-8\n\n")
+	if 'act' in q:
+		menu[q.getvalue('act')]()
+	else:
+		menu['Вывести весь список']()
